d3b61e3cc4dcbfe09e85e74486e7b80753fb7777
Baveltje01 <basvanrooten@me.com>
2018-05-29 00:01:38 +0200

    Tweaked MainActivity
    Also added Sofia font and reverted the default launch screen to activity main

--- HandpickedAndroidClient/app/src/main/java/com/example/jan_paul/handpickedandroidclient/Presentation/SplashActivity.java
+++ HandpickedAndroidClient/app/src/main/java/com/example/jan_paul/handpickedandroidclient/Presentation/SplashActivity.java
@@ -3,14 +3,18 @@ package com.example.jan_paul.handpickedandroidclient.Presentation;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.jan_paul.handpickedandroidclient.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static String TAG = "SplashActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
@@ -18,6 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.splash_loading_text);
         Typeface barlowLight = Typeface.createFromAsset(getAssets(),"fonts/barlow_light.ttf");
         textView.setTypeface(barlowLight);
+        Log.d(TAG, "onCreate: textView font changed to Barlow Light.");
 
     }
 }

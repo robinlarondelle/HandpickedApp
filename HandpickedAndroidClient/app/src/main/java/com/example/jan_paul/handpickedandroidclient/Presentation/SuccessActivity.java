c97d896a4fd800ec31a2e000c2f7b665f0015be4
Baveltje01 <basvanrooten@me.com>
2018-05-29 21:49:32 +0200

    Tweaked SplashActivity + Added new SuccessActivity

---
+++ HandpickedAndroidClient/app/src/main/java/com/example/jan_paul/handpickedandroidclient/Presentation/SuccessActivity.java
@@ -0,0 +1,25 @@
+package com.example.jan_paul.handpickedandroidclient.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.jan_paul.handpickedandroidclient.R;
+
+public class SuccessActivity extends AppCompatActivity {
+
+    private static String TAG = "SuccessActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+        Log.d(TAG, "onCreate: Initiated");
+
+
+
+
+
+    }
+}
